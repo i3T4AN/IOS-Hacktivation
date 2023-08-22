dfdb3f598cff4607b6c4bf503493230dc840888b
Ethan Blair <67167910+i3T4AN@users.noreply.github.com>
2023-08-22 09:40:33 -0600

    Update run.sh
    added port forwarding to -p 1111 for procusus bootstraps

--- bypass_scripts/mobileactivationd_12_4_7/run.sh
+++ bypass_scripts/mobileactivationd_12_4_7/run.sh
@@ -12,13 +12,13 @@ while true ; do
 echo 'CONNECTED TO DEVICE!'
 echo 'CONTINUE TO THE CHOOSE A WIFI NETWORK SCREEN BUT DO NOT CONNECT TO A NETWORK'
 read -p 'PRESS ENTER TO CONTINUE'
-sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 2222 mount -o rw,union,update /
-sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 2222 launchctl unload /System/Library/LaunchDaemons/com.apple.mobileactivationd.plist
-sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 2222 rm /usr/libexec/mobileactivationd
-sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 2222 uicache --all
+sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 1111 mount -o rw,union,update /
+sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 1111 launchctl unload /System/Library/LaunchDaemons/com.apple.mobileactivationd.plist
+sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 1111 rm /usr/libexec/mobileactivationd
+sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 1111 uicache --all
 sshpass -p 'alpine' scp -P 2222 bypass_scripts/mobileactivationd_12_4_7/mobileactivationd root@localhost:/usr/libexec/mobileactivationd
-sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 2222 chmod 755 /usr/libexec/mobileactivationd
-sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 2222 launchctl load /System/Library/LaunchDaemons/com.apple.mobileactivationd.plist
+sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 1111 chmod 755 /usr/libexec/mobileactivationd
+sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no root@localhost -p 1111 launchctl load /System/Library/LaunchDaemons/com.apple.mobileactivationd.plist
 pgrep -f 'tcprelay.py' | xargs kill >/dev/null 2>&1
 echo 'CHOOSE CONNECT TO ITUNES OPTION ON DEVICE TO COMPLETE BYPASS'
     break
